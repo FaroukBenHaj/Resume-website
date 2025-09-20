b91bae40b564ec642c826858d8f0d8fc95033a11
FaroukBenHaj <mohamedfarouk.benhajamor@esprit.tn>
2025-09-20 16:56:54 +0100

    feat/ added tailwind + shadcn

--- src/schemas/general.ts
+++ src/schemas/general.ts
@@ -3,8 +3,11 @@ export type Greneral = z.infer<ReturnType<typeof zGeneral>>;
 export const zGeneral = ()=>
     z.object({
         fullName: z.string(),
+        jobName:z.string(),
         uni: z.string(),
         country: z.string(),
         mail: z.string(),
         description: z.string(),
+
     });
+
