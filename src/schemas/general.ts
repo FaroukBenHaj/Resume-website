4ffe998fdb8d477e42423359abf543b7d49cd96e
FaroukBenHaj <mohamedfarouk.benhajamor@esprit.tn>
2025-09-21 20:00:28 +0100

    ui: added more design

--- src/schemas/general.ts
+++ src/schemas/general.ts
@@ -3,11 +3,15 @@ export type Greneral = z.infer<ReturnType<typeof zGeneral>>;
 export const zGeneral = ()=>
     z.object({
         fullName: z.string(),
-        jobName:z.string(),
         uni: z.string(),
         country: z.string(),
         mail: z.string(),
         description: z.string(),
+        actionbutton: z.object({
+            contact: z.string(),
+            cv: z.string(),
+      
+        }),
 
     });
 
