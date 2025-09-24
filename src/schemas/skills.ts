3b3af58393a7a1461658fb725db634c29c2cc9ab
Mohamed_Farouk_Ben_Haj_Amor <100999594+FaroukBenHaj@users.noreply.github.com>
2025-09-24 12:58:43 +0100

    Content skills (#17)
    * added : skills in the work experience
    
    * feat: added skills content

--- src/schemas/skills.ts
+++ src/schemas/skills.ts
@@ -3,9 +3,5 @@ export type Skills = z.infer<ReturnType<typeof zSkills>>;
 export const zSkills = ()=>
     z.object({
      name: z.string(),
-     media: z.object({
-        logo: z.string(),
-        alt: z.string(),
-     }),
     });
 
