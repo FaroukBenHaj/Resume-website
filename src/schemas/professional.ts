ca324acd455e77b820af83895f06502d05cfe85a
FaroukBenHaj <mohamedfarouk.benhajamor@esprit.tn>
2025-09-24 12:57:49 +0100

    feat: added skills content

--- src/schemas/professional.ts
+++ src/schemas/professional.ts
@@ -1,3 +1,4 @@
+import { reference } from "astro:content";
 import {z} from "astro:schema";
 export type Professional = z.infer<ReturnType<typeof zProfessional>>;
 export const zProfessional = ()=>
@@ -7,5 +8,6 @@ export const zProfessional = ()=>
         title: z.string(),
         year: z.number(),
         tasks: z.array(z.string()),
+        relatedSkills: z.array(reference("skills")),
     });
 
