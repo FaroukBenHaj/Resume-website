f54e80438b50a1a9bc1437c9dd5a5e83a93d533b
Mohamed_Farouk_Ben_Haj_Amor <100999594+FaroukBenHaj@users.noreply.github.com>
2025-09-24 12:07:56 +0100

    feat/workExperience
    * feat: added pro  content
    
    * ui : added shadcn template
    
    * ui : hero section enhancement
    
    * reftractor : updated the file structure
    
    * feat : work experience section + content
    
    * feat : added the  tasks of each internship
    
    * feat added skill content

---
+++ src/schemas/professional.ts
@@ -0,0 +1,11 @@
+import {z} from "astro:schema";
+export type Professional = z.infer<ReturnType<typeof zProfessional>>;
+export const zProfessional = ()=>
+    z.object({
+        company: z.string(),
+        duration: z.string() ,
+        title: z.string(),
+        year: z.number(),
+        tasks: z.array(z.string()),
+    });
+
